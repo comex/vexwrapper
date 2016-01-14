40a7098a3963f5138d48cde3c475f2a0bad0a2d6
comex <comexk@gmail.com>
2016-01-14 17:23:18 -0500

    fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
-#[derive(Copy, Clone, Show, PartialEq, Eq)]
+#![allow(non_upper_case_globals, dead_code)]
+extern crate libvex_sys;
+use libvex_sys::*;
+use std::ops::Deref;
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Signedness {
     Signed,
     Unsigned,
 }
 pub use Signedness::{Signed, Unsigned};
 
-#[derive(Copy, Clone, Show, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Binop {
     Add,
     Sub,
@@ -30,20 +34,20 @@ pub enum Binop {
     Wtf(u32),
 }
 
-enum Expr<Ref> {
+pub enum Expr<Ref> {
     Bin(Binop, Ref, Ref),
     Un(Unop, Ref),
     ITE(Ref, Ref, Ref),
     ConstInt(Ty, u64),
 }
 
-struct TExpr<Ref> {
-    ty: Ty,
-    ex: Expr<Ref>,
+pub struct TExpr<Ref> {
+    pub ty: Ty,
+    pub ex: Expr<Ref>,
 }
 
-#[derive(Copy, Clone, Show, PartialEq, Eq)]
-enum Ty {
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Ty {
     I1,
     I8,
     I16,
@@ -52,78 +56,81 @@ enum Ty {
     F32,
     F64,
 }
+pub use Ty::*;
 
 impl Ty {
     fn max_int_val(self) -> Option<u64> {
         match self {
             I1 =>  Some(1),
-            I8 =>  Some(std::u8::MAX),
-            I16 => Some(std::u16::MAX),
-            I32 => Some(std::u32::MAX),
+            I8 =>  Some(std::u8::MAX as u64),
+            I16 => Some(std::u16::MAX as u64),
+            I32 => Some(std::u32::MAX as u64),
             I64 => Some(std::u64::MAX),
             _ => None
         }
     }
     fn double_int(self) -> Option<Ty> {
         match self {
-            I8 =>  I16,
-            I16 => I32,
-            I32 => I64,
+            I8 =>  Some(I16),
+            I16 => Some(I32),
+            I32 => Some(I64),
             _ => None
         }
     }
 }
 
-fn guess_type<Ref: Deref<Target=&TyExpr>>(ex: Expr<Ref>) -> Ty {
+fn guess_type<Ref: RefTy>(ex: Expr<Ref>) -> Ty {
     match &ex {
-        &Bin(op, ref left, ref right) => {
+        &Expr::Bin(op, ref left, ref right) => {
             let lty = left.ty;
-            lef rty = right.ty;
+            let rty = right.ty;
             assert_eq!(lty, rty);
             match op {
                 Binop::WideningMul(_) => lty.double_int().unwrap(),
                 Binop::CmpEq | Binop::CmpNe | Binop::CmpLt(_) |
                 Binop::CmpLe(_) | Binop::CmpGt(_) | Binop::CmpGe(_) =>
-                    Ty::I1,
+                    I1,
                 _ => lty
             }
         },
-        &Un(op, ref arg) => {
+        &Expr::Un(op, ref arg) => {
             let ty = arg.ty;
             match op {
                 _ => ty
             }
         },
-        &ITE(ref iff, ref then, ref els) => {
-            assert_eq!(iff.ty, Ty::I1);
+        &Expr::ITE(ref iff, ref then, ref els) => {
+            assert_eq!(iff.ty, I1);
             assert_eq!(then.ty, els.ty);
             then.ty
         },
-        &ConstInt(ty, num) => {
+        &Expr::ConstInt(ty, num) => {
             assert!(num <= ty.max_int_val().expect("ConstInt ty not int"));
             ty
         },
     }
 }
 
+trait RefTy : Clone + Deref<Target=TExpr<Self>> {}
+
 trait Builder {
-    type Ref: Clone + Deref<Target=&TyExpr>;
-    fn build(&self, Expr<Ref>) -> Ref;
-    fn bin(&self, op: Binop, left: Ref, right: Ref) -> Ref {
+    type Ref: RefTy;
+    fn build(&self, Expr<Self::Ref>) -> Self::Ref;
+    fn bin(&self, op: Binop, left: Self::Ref, right: Self::Ref) -> Self::Ref {
         self.build(Expr::Bin(op, left, right))
     }
-    fn un(&self, op: Unop, arg: Ref) -> Ref {
+    fn un(&self, op: Unop, arg: Self::Ref) -> Self::Ref {
         self.build(Expr::Un(op, arg))
     }
-    fn ite(&self, iff: Ref, then: Ref, els: Ref) -> Ref {
+    fn ite(&self, iff: Self::Ref, then: Self::Ref, els: Self::Ref) -> Self::Ref {
         self.build(Expr::ITE(iff, then, els))
     }
-    fn const_int(&self, ty: Ty, num: u64) -> Ref {
+    fn const_int(&self, ty: Ty, num: u64) -> Self::Ref {
         self.build(Expr::ConstInt(ty, num))
     }
 }
 
-#[derive(Copy, Clone, Show, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Unop {
     Not,
     Neg,
@@ -134,16 +141,16 @@ fn make_unex<B: Builder>(b: B, op: u32, x: B::Ref) -> B::Ref {
     match op {
         Iop_Left8 | Iop_Left16 | Iop_Left32 | Iop_Left64 =>
             b.bin(Binop::Or, x.clone(), b.un(Unop::Neg, x.clone())),
-        Iop_CmpNEZ8 | Iop_CmpNEZ16 | Iop_CmpNEZ32 | Iop_CmpNEZ64 =>
-            b.bin(Binop::CmpNe, x, b.const_int(x.ty, 0)),
-        =>
-            
+        Iop_CmpNEZ8 | Iop_CmpNEZ16 | Iop_CmpNEZ32 | Iop_CmpNEZ64 => {
+            let ty = x.ty;
+            b.bin(Binop::CmpNe, x, b.const_int(ty, 0))
+        },
 
         _ => b.un(translate_unop(op), x),
     }
 }
 fn make_binex<B: Builder>(b: B, op: u32, l: B::Ref, r: B::Ref) -> B::Ref {
-    let cmp_ord = |sign: Signedness| {
+    let cmp_ord = |l: B::Ref, r: B::Ref, sign: Signedness| {
         let ty = l.ty;
         b.ite(b.bin(Binop::CmpLt(sign), l.clone(), r.clone()),
               b.const_int(ty, 8),
@@ -152,10 +159,10 @@ fn make_binex<B: Builder>(b: B, op: u32, l: B::Ref, r: B::Ref) -> B::Ref {
                     b.const_int(ty, 2)))
     };
     match op {
-        Iop_CmpORD32U | Iop_CmpORD64U => cmp_ord(Unsigned),
-        Iop_CmpORD32S | Iop_CmpORD64S => cmp_ord(Signed),
+        Iop_CmpORD32U | Iop_CmpORD64U => cmp_ord(l, r, Unsigned),
+        Iop_CmpORD32S | Iop_CmpORD64S => cmp_ord(l, r, Signed),
 
-        _ => b.bin(translate_unop(op), l, r),
+        _ => b.bin(translate_binop(op), l, r),
     }
 }
 
